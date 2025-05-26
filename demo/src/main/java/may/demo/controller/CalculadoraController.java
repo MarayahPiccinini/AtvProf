ad47e9ba6a69b048a2c5141b60e85d29bbe8b2c3
Marayah <marayah.pintos@sptech.school>
2025-05-26 20:03:01 -0300

    primeiro commit

---
+++ demo/src/main/java/may/demo/controller/CalculadoraController.java
@@ -0,0 +1,25 @@
+package may.demo.controller;
+
+import may.demo.Service.CalculadoraService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculadoraController {
+
+    private final CalculadoraService calculadoraService;
+
+    public CalculadoraController(CalculadoraService calculadoraService) {
+        this.calculadoraService = calculadoraService;
+    }
+
+
+    @PostMapping("/divisao/{a}/{b}")
+
+    public ResponseEntity<Double> divisao(@PathVariable double a, @PathVariable double b) {
+        return ResponseEntity.ok(calculadoraService.dividir(a, b));
+    }
+}
+
