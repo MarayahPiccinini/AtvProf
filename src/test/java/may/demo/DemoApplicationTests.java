d8c144ab10a5749061579ce1a47011d572edc0e7
Marayah <marayah.pintos@sptech.school>
2025-05-26 21:36:10 -0300

    adding tests

--- src/test/java/may/demo/DemoApplicationTests.java
+++ src/test/java/may/demo/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package may.demo;
 
+
+import may.demo.Service.CalculadoraService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+@Test
+public void testDividir(){
+		CalculadoraService calculadoraService = new CalculadoraService();
+		double a = 10;
+		double b = 2;
+		double esperado = 5;
 
+		double resultado = calculadoraService.dividir(a,b);
+
+		assertEquals(esperado, resultado);
+	}
 }
